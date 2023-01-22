1a6026b8bfbd37f26404eab1ee0814e5858dd8db
Sudipta Ghosh <sudipta002@gmail.com>
2023-01-22 12:16:03 +0530

    Update project name

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='prenlp',
+    name='preprocnlp',
     version='0.1',
     license='MIT',
     author="Sudipta Ghosh",
     author_email='sudipta002@gmail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/gmyrianthous/example-publish-pypi',
+    # url='https://github.com/gmyrianthous/example-publish-pypi',
     keywords='example project',
     install_requires=[
           'nltk',
